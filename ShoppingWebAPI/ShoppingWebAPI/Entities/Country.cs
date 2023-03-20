fee940a9490a06367fb400050512418316caa0c3
Hashenshing <juanpablo89correa@gmail.com>
2023-03-19 20:00:02 -0500

    New WebAPI Shopping project with new Country Entity

---
+++ ShoppingWebAPI/ShoppingWebAPI/Entities/Country.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShoppingWebAPI.Entities
+{
+    public class Country: Entity
+    {
+        [Display(Name = "País")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")] //NOT -- NULL
+        [MaxLength(50, ErrorMessage = "El campo {0} debe tener maximo {1} caracteres.")]
+        public string Name { get; set; }
+
+
+
+
+
+
+
+    }
+}
